92965fc7cbbccdf14c88eb204cc913e23d7eae37
futsalboss <arnoldwillsobong@gmail.com>
2021-12-07 12:42:33 +0100

    updated argument

--- work.py
+++ work.py
@@ -1,5 +1,7 @@
 def describe_city(name, country):
-    """tells us that lagos is a city in Nigeria"""
-    print(f"{name} is in {country}")
-describe_city("Lagos", "Nigeria")  
+    """tells us that Abuja is the capital of Nigeria"""
+    print(f"{name} is the capital of {country}")
+
+    
+describe_city("Abuja", "Nigeria")  
 
